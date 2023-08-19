10fefaec7e280922d2e372a8c53e3f9f5be5dda1
Juan Felipe Ciro Solórzano <jfciros@uqvirtual.edu.co>
2023-08-19 16:35:10 -0500

    Interfaz grafica, botones, apertura y cierre de ventanas

---
+++ src/main/java/com/example/guitallerRepasov2/MainController.java
@@ -0,0 +1,31 @@
+package com.example.guitallerRepasov2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+//import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController extends ActionEvent {
+    public Button btn_Cliente;
+
+
+
+
+   public void cliente() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("registroClientes.fxml"));
+       Parent root = loader.load();
+       Scene scene = new Scene(root);
+       Stage stage = new Stage();
+       stage.initModality(Modality.APPLICATION_MODAL);
+       stage.setScene(scene);
+       stage.showAndWait();
+
+    }
+}
